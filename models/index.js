de42b132a607b4f9311c5a3f1d8ed706f507e595
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-10-30 17:35:57 -0400

    implemented voting system: created vote model; create Put route for voting on post; updated Get routes to include votes; refactored code

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
+const Vote = require("./Vote");
+const Post = require("./Post.js");
 const User = require("./User.js");
-const Post = require("./Post.js")
+
 
 // create associations
 User.hasMany(Post, {
@@ -10,4 +12,34 @@ Post.belongsTo(User, {
     foreignKey: "user_id",
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+    through: Vote,
+    as: "voted_posts",
+    foreignKey: "user_id"
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: "voted_posts",
+    foreignKey: "post_id"
+});
+
+Vote.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: "post_id"
+});
+
+User.hasMany(Vote, {
+    foreignKey: "user_id"
+});
+
+User.hasMany(Vote, {
+    foreignKey: "post_id"
+});
+
+
+
+module.exports = { User, Post, Vote };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,18 @@
 const router = require ("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
+const sequelize = require("../../config/connection.js");
 
 // get all posts 
 router.get("/", (req, res) => {
     Post.findAll({
-        attributes: ["id", "post_url", "title", "created_at"],
+        attributes: [
+            "id",
+            "post_url",
+            "title", 
+            "created_at",
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+            'vote_count']
+        ],
         order: [["created_at", "DESC"]],
         include: [
             {
@@ -25,7 +33,14 @@ router.get("/:id", (req, res) => {
         where: {
             id: req.params.id 
         },
-        attributes: ["id", "post_url", "title", "created_at"],
+        attributes: [
+            "id",
+            "post_url",
+            "title", 
+            "created_at",
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+            'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -56,6 +71,17 @@ router.post("/", (req, res) => {
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
+        });
+});
+
+// PUT /api/posts/upvote
+router.put("/upvote", (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.json(err);
         });
 });
 
@@ -101,7 +127,6 @@ router.delete("/:id", (req, res) => {
             res.status(500).json(err);
         });
 });
-
 
 
 module.exports = router;
