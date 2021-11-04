0fee6b6619731b712fb5dda1b60a6a6fadf86405
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-11-03 22:54:37 -0400

    setup template engine; created homepage template and populated with Sequelize Data

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection.js");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
@@ -15,3 +21,6 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log("Now listening"));
 });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
